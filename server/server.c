b44c24ff7e3f3474ca5629157a9ae8f7a37834cf
Ashok Tamang <ashok.tamang@bison.howard.edu>
2017-02-11 17:51:36 -0500

    Implement of capitalize string and send back it to client.

--- server/server.c
+++ server/server.c
@@ -79,28 +79,28 @@ int main(int argc, char *argv[]) {
 	/*  Set all bytes in socket address structure to*/
 	/*zero, and fill in the relevant data members   */
 
-		/*memset(&servaddr, 0, sizeof(servaddr));*/
-		/*servaddr.sin_family      = AF_INET;*/
-		/*servaddr.sin_addr.s_addr = htonl(INADDR_ANY);*/
-		/*servaddr.sin_port        = htons(port);*/
+	/*memset(&servaddr, 0, sizeof(servaddr));*/
+	/*servaddr.sin_family      = AF_INET;*/
+	/*servaddr.sin_addr.s_addr = htonl(INADDR_ANY);*/
+	/*servaddr.sin_port        = htons(port);*/
 
 
-		/*  Bind our socket addresss to the */
-		/*listening socket, and call listen()  */
+	/*  Bind our socket addresss to the */
+	/*listening socket, and call listen()  */
 
-		/*if ( bind(list_s, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0 ) {*/
-		/*fprintf(stderr, "ECHOSERV: Error calling bind()\n");*/
-		/*exit(EXIT_FAILURE);*/
-		/*}*/
+	/*if ( bind(list_s, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0 ) {*/
+	/*fprintf(stderr, "ECHOSERV: Error calling bind()\n");*/
+	/*exit(EXIT_FAILURE);*/
+	/*}*/
 
-		/*if ( listen(list_s, LISTENQ) < 0 ) {*/
-		/*perror("ECHOSERV: Error calling listen()\n");*/
-		/*exit(EXIT_FAILURE);*/
-		/*}*/
+	/*if ( listen(list_s, LISTENQ) < 0 ) {*/
+	/*perror("ECHOSERV: Error calling listen()\n");*/
+	/*exit(EXIT_FAILURE);*/
+	/*}*/
 
-		/*UDP Connection  */
+	/*UDP Connection  */
 
-		socket_udp = socket(AF_INET, SOCK_DGRAM, 0);
+	socket_udp = socket(AF_INET, SOCK_DGRAM, 0);
 
 	/*  Set all bytes in socket address structure to
 	    zero, and fill in the relevant data members   */
@@ -140,117 +140,109 @@ int main(int argc, char *argv[]) {
 		/*exit(EXIT_FAILURE);*/
 		/*}*/
 
-		/*Retrieve an input line from the connected socket*/
-		/*then simply write it back to the  same socket.     */
-		/**/
 
-			/*while (1) {*/
-			/*read(socket_tcp, buffer, MAX_LINE-1);*/
+		if (strncmp(buffer, "CAP", 3) == 0) {
+		/*number of relevant bytes of message */
+		/*= buffer length - 'CAP' length - length of two line breaks - end of string */
 
-			/*if (strncmp(buffer, "CAP", 3) == 0) {*/
-			/* number of relevant bytes of message */
-			/*= buffer length - 'CAP' length - length of two line breaks - end of string */
-			/**/
-
-			/*to_capitalize =  (char* ) malloc(sizeof(char*) * (strlen(buffer) - 6));*/
-			/*memcpy(to_capitalize, buffer + 4, strlen(buffer) - 6);*/
+			to_capitalize =  (char* ) malloc(sizeof(char*) * (strlen(buffer) - 6));
+			memcpy(to_capitalize, buffer + 4, strlen(buffer) - 6);
 
 			/*[> Capitalize the messsage <]*/
-			/*int index = 0;*/
-			/*while (to_capitalize[index] != '\0') {*/
-			/*if (islower(to_capitalize[index])) {*/
-			/*to_capitalize[index] = toupper(to_capitalize[index]);*/
-			/*}*/
-			/*else {*/
-			/*to_capitalize[index] = to_capitalize[index];*/
-			/*}*/
-			/*index++;*/
-			/*}*/
+			int index = 0;
+			while (to_capitalize[index] != '\0') {
+				if (islower(to_capitalize[index])) {
+					to_capitalize[index] = toupper(to_capitalize[index]);
+				}
+				else {
+					to_capitalize[index] = to_capitalize[index];
+				}
+				index++;
+			}
 
 			/*[> parse the capitalized message to send to the client <]*/
 
-			/*sprintf(buffer_send, "%d", strlen(to_capitalize));*/
-			/*strcat(buffer_send, "\n");*/
-			/*strcat(buffer_send, to_capitalize);*/
+			sprintf(buffer_send, "%s", to_capitalize);
+			strcat(buffer_send, "\n");
+			strcat(buffer_send, to_capitalize);
 
 			/*[> send the formatted message to the client <]*/
 			/*write(socket_tcp, buffer_send, strlen(buffer_send));*/
 
 			/*[> free the memory <]*/
-			/*free(to_capitalize);*/
+			free(to_capitalize);
 
-			/*}*/
+		}
 
-			/*if (strncmp(buffer, "FILE", 4) == 0) {*/
-			/*file_name = (char* ) malloc (sizeof(char*) * (strlen(buffer) - 6));*/
-			/*memcpy(file_name, buffer + 5, strlen(buffer) - 6);*/
+		/*if (strncmp(buffer, "FILE", 4) == 0) {*/
+		/*file_name = (char* ) malloc (sizeof(char*) * (strlen(buffer) - 6));*/
+		/*memcpy(file_name, buffer + 5, strlen(buffer) - 6);*/
 
-			/*[> Find file name and read that file <]*/
-			/*fp = fopen(file_name, "rb");*/
-			/*if (fp) {*/
-			/*long lSize;*/
-			/*void* large_buffer;*/
-			/*size_t result;*/
+		/*[> Find file name and read that file <]*/
+		/*fp = fopen(file_name, "rb");*/
+		/*if (fp) {*/
+		/*long lSize;*/
+		/*void* large_buffer;*/
+		/*size_t result;*/
 
-			/*[> obtain file size <]*/
-			/*fseek(fp, 0, SEEK_END);*/
-			/*lSize = ftell(fp);*/
-			/*rewind(fp); [> Put the postion of pointer back to the start of the file <]*/
+		/*[> obtain file size <]*/
+		/*fseek(fp, 0, SEEK_END);*/
+		/*lSize = ftell(fp);*/
+		/*rewind(fp); [> Put the postion of pointer back to the start of the file <]*/
 
-			/*[> allocate memory to hold the whole file <]*/
-			/*large_buffer = (void* ) malloc(sizeof(void*) * lSize);*/
-			/*if (large_buffer == NULL) {*/
-			/*perror("Error in allocating required memory: ");*/
-			/*}*/
+		/*[> allocate memory to hold the whole file <]*/
+		/*large_buffer = (void* ) malloc(sizeof(void*) * lSize);*/
+		/*if (large_buffer == NULL) {*/
+		/*perror("Error in allocating required memory: ");*/
+		/*}*/
 
-			/*[> copy the file into the buffer <]*/
-			/*result = fread(large_buffer, 1, lSize, fp);*/
-			/*if (result != lSize) {*/
-			/*printf("Error reading whole file.\n");*/
-			/*}*/
+		/*[> copy the file into the buffer <]*/
+		/*result = fread(large_buffer, 1, lSize, fp);*/
+		/*if (result != lSize) {*/
+		/*printf("Error reading whole file.\n");*/
+		/*}*/
 
-			/*[> close the file and later free the large_buffer <]*/
-			/*fclose(fp);*/
+		/*[> close the file and later free the large_buffer <]*/
+		/*fclose(fp);*/
 
-			/*[> send the buffer to the client <]*/
-			/*if (MAX_LINE < lSize) {*/
-			/*strcpy(buffer_send, "The buffer size is smaller than what needs to be sent.");*/
-			/*write(socket_tcp, buffer_send, strlen(buffer_send));*/
-			/*}*/
-			/*else {*/
-			/*sprintf(buffer_send, "%d", lSize);*/
-			/*strcat(buffer_send, "\n");*/
-			/*strcat(buffer_send, large_buffer);*/
-			/*write(socket_tcp, buffer_send, lSize);*/
-			/*}*/
+		/*[> send the buffer to the client <]*/
+		/*if (MAX_LINE < lSize) {*/
+		/*strcpy(buffer_send, "The buffer size is smaller than what needs to be sent.");*/
+		/*write(socket_tcp, buffer_send, strlen(buffer_send));*/
+		/*}*/
+		/*else {*/
+		/*sprintf(buffer_send, "%d", lSize);*/
+		/*strcat(buffer_send, "\n");*/
+		/*strcat(buffer_send, large_buffer);*/
+		/*write(socket_tcp, buffer_send, lSize);*/
+		/*}*/
 
-			/*[> free the memory <]*/
-			/*free(large_buffer);*/
-			/*free(file_name);*/
-			/*} else {*/
-			/*[> No such file <]*/
-			/*strcpy(buffer, "NOT FOUND");*/
-			/*sprintf(buffer_send, "%d", strlen(buffer));*/
-			/*strcat(buffer_send, "\n");*/
-			/*strcat(buffer_send, buffer);*/
+		/*[> free the memory <]*/
+		/*free(large_buffer);*/
+		/*free(file_name);*/
+		/*} else {*/
+		/*[> No such file <]*/
+		/*strcpy(buffer, "NOT FOUND");*/
+		/*sprintf(buffer_send, "%d", strlen(buffer));*/
+		/*strcat(buffer_send, "\n");*/
+		/*strcat(buffer_send, buffer);*/
 
-			/*[> Inform client that file is not in the server. <]*/
-			/*write(socket_tcp, buffer_send, strlen(buffer_send));*/
-			/*}*/
+		/*[> Inform client that file is not in the server. <]*/
+		/*write(socket_tcp, buffer_send, strlen(buffer_send));*/
+		/*}*/
 
-			/*}*/
+		/*}*/
 
-			/*   [>* free the memory */
-			memset(buffer, 0, (sizeof buffer[0]) * MAX_LINE);
+		/*   [>* free the memory */
+		memset(buffer, 0, (sizeof buffer[0]) * MAX_LINE);
 		memset(buffer_send, 0, (sizeof buffer_send[0]) * MAX_LINE);
-	}
 
-	/*  Close the connected socket  */
-	/*         if ( close(socket_tcp) < 0 ) {*/
-	/*perror("ECHOSERV: Error calling close()\n");*/
-	/*exit(EXIT_FAILURE);*/
-	/*}*/
-	/*else {*/
-	/*fprintf(stderr, "Connection closed.\n");*/
-	/*}*/
-}
+		/*  Close the connected socket  */
+		/*         if ( close(socket_tcp) < 0 ) {*/
+		/*perror("ECHOSERV: Error calling close()\n");*/
+		/*exit(EXIT_FAILURE);*/
+		/*}*/
+		/*else {*/
+		/*fprintf(stderr, "Connection closed.\n");*/
+		/*}*/
+	}
